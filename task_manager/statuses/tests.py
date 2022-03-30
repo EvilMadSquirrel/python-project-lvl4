c58944de25ab65150b041b67285081c81eca394f
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-03-30 13:48:15 +0300

    modified tests for statuses (disable deleting status with tasks)

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -3,16 +3,18 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from django.contrib.auth.models import User
+from task_manager.tasks.models import Task
 from .models import Status
 
 
 class TestStatuses(TestCase):
-    fixtures = ["statuses.json", "users.json"]
+    fixtures = ["statuses.json", "users.json", "tasks.json"]
 
     def setUp(self) -> None:
         self.user = User.objects.get(pk=1)
         self.status1 = Status.objects.get(pk=1)
         self.status2 = Status.objects.get(pk=2)
+        self.task1 = Task.objects.get(pk=1)
 
     def test_statuses_list(self):
         self.client.force_login(self.user)
@@ -52,8 +54,17 @@ class TestStatuses(TestCase):
         self.assertContains(response, _("Status changed successfully"))
         self.assertEqual(Status.objects.get(pk=self.status1.id), self.status1)
 
+    def test_status_with_tasks_delete(self):
+        self.client.force_login(self.user)
+        url = reverse("statuses:delete", args=(self.status1.id,))
+        response = self.client.post(url, follow=True)
+        self.assertTrue(User.objects.filter(pk=self.status1.id).exists())
+        self.assertRedirects(response, "/statuses/")
+        self.assertContains(response, _("Cannot delete status because it is in use"))
+
     def test_delete_status(self):
         self.client.force_login(self.user)
+        self.task1.delete()
         url = reverse("statuses:delete", args=(self.status1.id,))
         response = self.client.post(url, follow=True)
         # noinspection PyTypeChecker
